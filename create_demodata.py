7fec53c5a641b29678c32dc191a3a2bf5c5ddd94
TeemuBergman <68549594+TeemuBergman@users.noreply.github.com>
2020-08-09 23:59:29 +0300

    Update create_demodata.py
    Bug fixes

--- create_demodata.py
+++ create_demodata.py
@@ -1,10 +1,9 @@
-# commands.py
+# create_demodata.py
 
 import os
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .messageboard.db_models import Categories
-from .messageboard.tools import getTime
 
 
 app = Flask(__name__)
@@ -13,20 +12,29 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+# Defining category model
+class Categories(db.Model):
+    __tablename__ = "categories"
+    category_id = db.Column(db.Integer, primary_key = True)
+    category_secret = db.Column(db.Boolean, nullable = False)
+    category_name = db.Column(db.String(256), unique = True, nullable = False)
+    category_description = db.Column(db.String(256), nullable = False)
+    category_created = db.Column(db.DateTime, nullable = False)
+
 # Create demo data for database
 
 general = Categories(
     category_name = "General",
     category_description = "General discussion about almost everything",
     category_secret = False,
-    category_created = getTime()
+    category_created = datetime.now()
 )
 
 coding = Categories(
     category_name = "Coding",
     category_description = "Ask about coding here!",
     category_secret = False,
-    category_created = getTime()
+    category_created = datetime.now()
 )
 
 db.session.add(general, coding)
