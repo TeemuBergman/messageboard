36e7e1679d69aadce5b4ddc8dec6e299a5617ca6
746162 <60036511+746162@users.noreply.github.com>
2020-08-23 10:23:12 +0300

    Updated database model on users

--- messageboard/db_models.py
+++ messageboard/db_models.py
@@ -1,3 +1,5 @@
+# db_models.py
+
 from flask_login import UserMixin
 from . import db
 
@@ -7,13 +9,15 @@ class Users(UserMixin, db.Model):
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), nullable = False)
-    email = db.Column(db.String(256), nullable = False, unique = True)
     password_hash = db.Column(db.String(256), nullable = False)
+    email = db.Column(db.String(256), nullable = False, unique = True)
     account_created = db.Column(db.DateTime, nullable = False)
     last_login = db.Column(db.DateTime, nullable = False)
     banned = db.Column(db.Boolean, nullable = False)
     ban_duration = db.Column(db.DateTime, nullable = False)
-    is_admin = db.Column(db.Boolean, nullable = False)
+    is_admin = db.Column(db.Boolean, nullable = False, default = False)
+    can_view_secret = db.Column(db.Boolean, nullable = False, default = False)
+    deleted = db.Column(db.Boolean, nullable = False, default = False)
 
     def get_id(self):
         return self.user_id
@@ -36,7 +40,7 @@ class Threads(db.Model):
     thread_visible = db.Column(db.Boolean, nullable = False)
     thread_name = db.Column(db.String(256), unique = True, nullable = False)
     thread_created = db.Column(db.DateTime, nullable = False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.category_id"))
 
 
 # Defining message model
@@ -45,5 +49,5 @@ class Messages(db.Model):
     message_id = db.Column(db.Integer, primary_key = True)
     message_content = db.Column(db.String, nullable = False)
     message_created = db.Column(db.DateTime, nullable = False)
-    thread_id = db.Column(db.Integer, db.ForeignKey('threads.thread_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    thread_id = db.Column(db.Integer, db.ForeignKey("threads.thread_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
