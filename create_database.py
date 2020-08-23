d917c94526b7bae802ec35b43e9635739d8229b8
746162 <60036511+746162@users.noreply.github.com>
2020-08-23 11:24:24 +0300

    Fixed a bug with DB model imports

--- create_database.py
+++ create_database.py
@@ -1,7 +1,10 @@
 # create_database.py
 
+from flask import Flask
 from messageboard.db_models import Categories, Threads, Messages, Users
 from messageboard import db
+
+app = Flask(__name__)
 
 db.session.add(Categories)
 db.session.add(Threads)
