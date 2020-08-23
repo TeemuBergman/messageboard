65e3088092b823d0c6d69acae7ea1cf7c3e80ec4
746162 <60036511+746162@users.noreply.github.com>
2020-08-23 09:21:36 +0300

    Configs cleaned

--- config.py
+++ config.py
@@ -7,26 +7,11 @@ class Config:
     # Config variables
     TESTING = False
     DEBUG = False
-    FLASK_ENV = 'development'
-    SECRET_KEY = getenv('SECRET_KEY')
-    # SESSION_COOKIE_NAME = getenv('SESSION_COOKIE_NAME')
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
+    FLASK_ENV = "development"
+    SECRET_KEY = getenv("SECRET_KEY")
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
 
     # Database config
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
-
-
-class ProdConfig(Config):
-    FLASK_ENV = 'production'
-    DEBUG = False
-    TESTING = False
-    # DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
-
-
-class DevConfig(Config):
-    FLASK_ENV = 'development'
-    DEBUG = True
-    TESTING = True
-    # DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
