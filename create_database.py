93be19c4bc9f2326279cc10165315bb04edc248f
746162 <60036511+746162@users.noreply.github.com>
2020-08-23 11:14:08 +0300

    Fixed a bug with DB model imports

--- create_database.py
+++ create_database.py
@@ -1,18 +1,10 @@
 # create_database.py
 
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from messageboard.db_models import Categories, Threads, Messages, Users
+from messageboard import db
 
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-Categories.db.create_all()
-Threads.db.create_all()
-Messages.db.create_all()
-Users.db.create_all()
+db.session.add(Categories)
+db.session.add(Threads)
+db.session.add(Messages)
+db.session.add(Users)
+db.session.commit()
